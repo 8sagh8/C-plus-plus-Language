d475bfb24470a46e1b1de4dc2966da646ee06207
Sammar Abbas <sabbas21@myseneca.ca>
2019-11-07 11:52:25 -0500

    edited

--- Algorithm/Sort/sort_Alorithm_edited.cpp
+++ Algorithm/Sort/sort_Alorithm_edited.cpp
@@ -1,4 +1,3 @@
-
 // Algorithms - Sort Descending Order
 // sort.cpp
 
@@ -7,26 +6,35 @@
 using namespace std;
 
 int main() {
+	// we have an array but don't know its size
 	int a[] = { 3, 2, 4, 1, 10, 0, -5, 6, 19 };
-	int b = sizeof(a) / sizeof(int);
+
+	// calculating size of an array
+	int b = sizeof(a) / sizeof(int); 
+
+
+
+	cout << "*******Descending Order*************" << endl << endl;
 
 	std::sort(a, &a[b], [](int i, int j) { return i > j; });
 	for (int i : a)
 		std::cout << i << std::endl;
 
-	cout << "=================" << endl << endl;
-
+	cout << "*******Ascending Order*************" << endl << endl;
+	// don't need lambda.... by just this we can get ascending order
 	sort(a, &a[b]);
 
 	for (int i : a)
-	cout << i << endl;
+		cout << i << endl;
 
 	cout << "=================" << endl << endl;
-	
+
+	cout << "If we write own function to get descending order" << endl << endl;
+
 	int i = 0;
-	
+
 	while (i < (sizeof(a) / sizeof(int))) {
-		for (int j = i+1; j < (sizeof(a) / sizeof(int)); ++j) {
+		for (int j = i + 1; j < (sizeof(a) / sizeof(int)); ++j) {
 			if (a[i] < a[j]) {
 				int k = a[i];
 				a[i] = a[j];
@@ -39,7 +47,7 @@ int main() {
 	i = 0;
 
 	while (i < (sizeof(a) / sizeof(int))) {
-		
+
 		cout << a[i] << endl;
 		++i;
 	}
