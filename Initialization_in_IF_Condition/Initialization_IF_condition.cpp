1b1220387ff41a08889860d66093ddec71ec0400
8sagh8 <mastersagh@hotmail.com>
2019-11-20 12:30:37 -0500

    if

---
+++ Initialization_in_IF_Condition/Initialization_IF_condition.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+	unsigned i;
+	std::cout << "Enter i : ";
+	std::cin >> i;
+	
+	// initializing variable "j" in if ();
+	if (unsigned j = i % 10u; j < 5u) { // C++17
+		i -= j;      // round down
+	}
+	else {
+		i += 10u - j; // round up
+	}
+	std::cout << i << std::endl;
+
+	std::cout << "Enter i : ";
+	std::cin >> i;
+	switch (unsigned j = i % 10u; j / 5u) { // C++17
+	case 0u: // round down
+		i -= j;
+		break;
+	case 1u: // round up
+		i += 10u - j;
+	}
+	std::cout << i << std::endl;
+}
